573001463b2df849b5a5fa951084933b651ddb1a
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-04-06 16:36:21 -0600

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Ricardo Camarena on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
